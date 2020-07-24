9f4cbe2e29867595a068777753c97baff32b201e
Alexander <sashok935@gmail.com>
2020-07-25 02:46:06 +0300

    Fix deployment errors

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-
 set -o errexit
 
 cd /opt/despair-bot || { echo "No directory found"; exit 1; }
-
 git pull
-
 npm install
-
+rm -rf dist/
 npm run build
-
 mv dist/src/* dist/
-
 rm -rf dist/src dist/package.json
-
 pm2 reload d-bot
