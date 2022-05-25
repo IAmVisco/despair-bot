ce8003388e81149d04d6a4fe52b5d187cad0fbf9
Alexander <sashok935@gmail.com>
2022-05-25 12:23:21 +0400

    Bump deps

--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -1,6 +1,6 @@
-import * as Redis from 'ioredis';
+import Redis from 'ioredis';
 
-const redis = new Redis(process.env.REDIS_PORT);
+const redis = new Redis(parseInt(process.env.REDIS_PORT as string, 10) || 6379);
 redis.config('SET', 'notify-keyspace-events', 'KEA');
 
 export { redis };
